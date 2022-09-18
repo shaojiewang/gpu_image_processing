78498a8deb037e70648df21ced5c8ee0f56e18dc
shaojiewang <wsjmessi@163.com>
2022-09-19 00:26:26 +0800

    use outer product

--- host/gemm_cpu_opt2.hpp
+++ host/gemm_cpu_opt2.hpp
@@ -12,38 +12,41 @@ template<typename ADataType,
 static inline void gemm_cpu_macro_tile_4x16(const ADataType* APtr, const BDataType* BPtr, CDataType* CPtr, int M, int N, int K)
 {
     //const BDataType* BTmp = BPtr;
-    __m512 v_b_k0_f_16, v_b_k1_f_16, v_b_k2_f_16, v_b_k3_f_16, v_c_f_16;
-    __m512 v_a_k0_f_16, v_a_k1_f_16, v_a_k2_f_16, v_a_k3_f_16;
-    __m128 v_a_f_4, v_a_f_k0_4, v_a_f_k1_4, v_a_f_k2_4, v_a_f_k3_4;
-    for(int i = 0; i < MTile; i += 1)
+    __m512 v_b_f_16;
+    __m512 v_c_i0_f_16, v_c_i1_f_16, v_c_i2_f_16, v_c_i3_f_16;
+    __m512 v_a_i0_f_16, v_a_i1_f_16, v_a_i2_f_16, v_a_i3_f_16;
+    __m128 v_a_f_i0_4, v_a_f_i1_4, v_a_f_i2_4, v_a_f_i3_4;
+    for(int k = 0; k < KTile; k += 1)
     {
-        for(int k = 0; k < KTile; k += 4)
+        for(int i = 0; i < MTile; i += 4)
         {
             //ADataType AData = APtr[i * K + k];
-            v_a_f_4 = _mm_load_ps(APtr + i * K + k);
-            v_a_f_k0_4 = _mm_shuffle_ps(v_a_f_4, v_a_f_4, 0x0);
-            v_a_f_k1_4 = _mm_shuffle_ps(v_a_f_4, v_a_f_4, 0x55);
-            v_a_f_k2_4 = _mm_shuffle_ps(v_a_f_4, v_a_f_4, 0xaa);
-            v_a_f_k3_4 = _mm_shuffle_ps(v_a_f_4, v_a_f_4, 0xff);
-            v_a_k0_f_16 = _mm512_broadcast_f32x2(v_a_f_k0_4);
-            v_a_k1_f_16 = _mm512_broadcast_f32x2(v_a_f_k1_4);
-            v_a_k2_f_16 = _mm512_broadcast_f32x2(v_a_f_k2_4);
-            v_a_k3_f_16 = _mm512_broadcast_f32x2(v_a_f_k3_4);
+            v_a_f_i0_4 = _mm_broadcast_ss(APtr + i * K + k);
+            v_a_f_i1_4 = _mm_broadcast_ss(APtr + (i + 1) * K + k);
+            v_a_f_i2_4 = _mm_broadcast_ss(APtr + (i + 2) * K + k);
+            v_a_f_i3_4 = _mm_broadcast_ss(APtr + (i + 3) * K + k);
+            v_a_i0_f_16 = _mm512_broadcast_f32x2(v_a_f_i0_4);
+            v_a_i1_f_16 = _mm512_broadcast_f32x2(v_a_f_i1_4);
+            v_a_i2_f_16 = _mm512_broadcast_f32x2(v_a_f_i2_4);
+            v_a_i3_f_16 = _mm512_broadcast_f32x2(v_a_f_i3_4);
             for(int j = 0; j < NTile; j += 16)
             {   
-                v_b_k0_f_16 = _mm512_load_ps(BPtr + k * N + j);
-                v_b_k1_f_16 = _mm512_load_ps(BPtr + (k + 1) * N + j);
-                v_b_k2_f_16 = _mm512_load_ps(BPtr + (k + 2) * N + j);
-                v_b_k3_f_16 = _mm512_load_ps(BPtr + (k + 3 ) * N + j);
+                v_b_f_16 = _mm512_load_ps(BPtr + k * N + j);
 
-                v_c_f_16 = _mm512_load_ps(CPtr + i * N + j);
+                v_c_i0_f_16 = _mm512_load_ps(CPtr + i * N + j);
+                v_c_i1_f_16 = _mm512_load_ps(CPtr + (i + 1) * N + j);
+                v_c_i2_f_16 = _mm512_load_ps(CPtr + (i + 2) * N + j);
+                v_c_i3_f_16 = _mm512_load_ps(CPtr + (i + 3) * N + j);
 
-                v_c_f_16 = _mm512_fmadd_ps(v_a_k0_f_16, v_b_k0_f_16, v_c_f_16);
-                v_c_f_16 = _mm512_fmadd_ps(v_a_k1_f_16, v_b_k1_f_16, v_c_f_16);
-                v_c_f_16 = _mm512_fmadd_ps(v_a_k2_f_16, v_b_k2_f_16, v_c_f_16);
-                v_c_f_16 = _mm512_fmadd_ps(v_a_k3_f_16, v_b_k3_f_16, v_c_f_16);
+                v_c_i0_f_16 = _mm512_fmadd_ps(v_a_i0_f_16, v_b_f_16, v_c_i0_f_16);
+                v_c_i1_f_16 = _mm512_fmadd_ps(v_a_i1_f_16, v_b_f_16, v_c_i1_f_16);
+                v_c_i2_f_16 = _mm512_fmadd_ps(v_a_i2_f_16, v_b_f_16, v_c_i2_f_16);
+                v_c_i3_f_16 = _mm512_fmadd_ps(v_a_i3_f_16, v_b_f_16, v_c_i3_f_16);
 
-                _mm512_store_ps(CPtr + i * N + j, v_c_f_16);
+                _mm512_store_ps(CPtr + i * N + j, v_c_i0_f_16);
+                _mm512_store_ps(CPtr + (i + 1) * N + j, v_c_i1_f_16);
+                _mm512_store_ps(CPtr + (i + 2) * N + j, v_c_i2_f_16);
+                _mm512_store_ps(CPtr + (i + 3) * N + j, v_c_i3_f_16);
                 //CPtr[i * N + j] += AData * BPtr[k * N + j];
             }
         }
