57460b89f7b5b6ba4695cf5dd59a03e5e1e67f2b
shaojiewang <wsjmessi@163.com>
2022-09-30 19:48:25 +0800

    use micro kernel. now 4x32x32 tile size. got 39.9ms

--- host/gemm_cpu_opt2.hpp
+++ host/gemm_cpu_opt2.hpp
@@ -5,51 +5,98 @@
 template<typename ADataType,
          typename BDataType,
          typename CDataType,
+         typename AccDataType>
+static inline void gemm_cpu_micro_tile_4x32x32(const ADataType* APtr, const BDataType* BPtr, CDataType* CPtr, int M, int N, int K)
+{
+    __m512 v_b_n0_f_16, v_b_n1_f_16;
+    __m512 v_c_m0n0_f_16, v_c_m1n0_f_16, v_c_m2n0_f_16, v_c_m3n0_f_16;
+    __m512 v_c_m0n1_f_16, v_c_m1n1_f_16, v_c_m2n1_f_16, v_c_m3n1_f_16;
+    __m512 v_a_m0_f_16, v_a_m1_f_16, v_a_m2_f_16, v_a_m3_f_16;
+    __m128 v_a_m0_f_4, v_a_m1_f_4, v_a_m2_f_4, v_a_m3_f_4;
+
+    CDataType* CTmp0 = CPtr;
+    CDataType* CTmp1 = CTmp0 + N;
+    CDataType* CTmp2 = CTmp1 + N;
+    CDataType* CTmp3 = CTmp2 + N;
+
+    CDataType* CTmp4 = CTmp0 + 16;
+    CDataType* CTmp5 = CTmp1 + 16;
+    CDataType* CTmp6 = CTmp2 + 16;
+    CDataType* CTmp7 = CTmp3 + 16;
+
+    const ADataType* ATmp = APtr;
+
+    v_c_m0n0_f_16 = _mm512_load_ps(CTmp0);
+    v_c_m1n0_f_16 = _mm512_load_ps(CTmp1);
+    v_c_m2n0_f_16 = _mm512_load_ps(CTmp2);
+    v_c_m3n0_f_16 = _mm512_load_ps(CTmp3);
+
+    v_c_m0n1_f_16 = _mm512_load_ps(CTmp4);
+    v_c_m1n1_f_16 = _mm512_load_ps(CTmp5);
+    v_c_m2n1_f_16 = _mm512_load_ps(CTmp6);
+    v_c_m3n1_f_16 = _mm512_load_ps(CTmp7); 
+
+    for(int k = 0; k < 32; k++)
+    {
+        ATmp = APtr;
+        v_a_m0_f_4 = _mm_broadcast_ss(ATmp); ATmp += K;
+        v_a_m1_f_4 = _mm_broadcast_ss(ATmp); ATmp += K;
+        v_a_m2_f_4 = _mm_broadcast_ss(ATmp); ATmp += K;
+        v_a_m3_f_4 = _mm_broadcast_ss(ATmp);
+        v_a_m0_f_16 = _mm512_broadcast_f32x2(v_a_m0_f_4);
+        v_a_m1_f_16 = _mm512_broadcast_f32x2(v_a_m1_f_4);
+        v_a_m2_f_16 = _mm512_broadcast_f32x2(v_a_m2_f_4);
+        v_a_m3_f_16 = _mm512_broadcast_f32x2(v_a_m3_f_4);
+
+        v_b_n0_f_16 = _mm512_load_ps(BPtr); BPtr += 16;
+        v_b_n1_f_16 = _mm512_load_ps(BPtr); BPtr += N - 16;
+
+        v_c_m0n0_f_16 = _mm512_fmadd_ps(v_a_m0_f_16, v_b_n0_f_16, v_c_m0n0_f_16);
+        v_c_m1n0_f_16 = _mm512_fmadd_ps(v_a_m1_f_16, v_b_n0_f_16, v_c_m1n0_f_16);
+        v_c_m2n0_f_16 = _mm512_fmadd_ps(v_a_m2_f_16, v_b_n0_f_16, v_c_m2n0_f_16);
+        v_c_m3n0_f_16 = _mm512_fmadd_ps(v_a_m3_f_16, v_b_n0_f_16, v_c_m3n0_f_16);
+
+        v_c_m0n1_f_16 = _mm512_fmadd_ps(v_a_m0_f_16, v_b_n1_f_16, v_c_m0n1_f_16);
+        v_c_m1n1_f_16 = _mm512_fmadd_ps(v_a_m1_f_16, v_b_n1_f_16, v_c_m1n1_f_16);
+        v_c_m2n1_f_16 = _mm512_fmadd_ps(v_a_m2_f_16, v_b_n1_f_16, v_c_m2n1_f_16);
+        v_c_m3n1_f_16 = _mm512_fmadd_ps(v_a_m3_f_16, v_b_n1_f_16, v_c_m3n1_f_16);
+
+        APtr++;
+    }
+    _mm512_store_ps(CTmp0, v_c_m0n0_f_16);
+    _mm512_store_ps(CTmp1, v_c_m1n0_f_16);
+    _mm512_store_ps(CTmp2, v_c_m2n0_f_16);
+    _mm512_store_ps(CTmp3, v_c_m3n0_f_16);
+
+    _mm512_store_ps(CTmp4, v_c_m0n1_f_16);
+    _mm512_store_ps(CTmp5, v_c_m1n1_f_16);
+    _mm512_store_ps(CTmp6, v_c_m2n1_f_16);
+    _mm512_store_ps(CTmp7, v_c_m3n1_f_16);
+}
+
+template<typename ADataType,
+         typename BDataType,
+         typename CDataType,
          typename AccDataType,
          int MTile,
          int NTile,
          int KTile>
-static inline void gemm_cpu_macro_tile_4x16(const ADataType* APtr, const BDataType* BPtr, CDataType* CPtr, int M, int N, int K)
+static inline void gemm_cpu_macro_tile_M_N_K(const ADataType* APtr, const BDataType* BPtr, CDataType* CPtr, int M, int N, int K)
 {
-    //const BDataType* BTmp = BPtr;
-    __m512 v_b_f_16;
-    __m512 v_c_i0_f_16, v_c_i1_f_16, v_c_i2_f_16, v_c_i3_f_16;
-    __m512 v_a_i0_f_16, v_a_i1_f_16, v_a_i2_f_16, v_a_i3_f_16;
-    __m128 v_a_f_i0_4, v_a_f_i1_4, v_a_f_i2_4, v_a_f_i3_4;
-    for(int k = 0; k < KTile; k += 1)
+    const ADataType* ATmp = APtr;
+    const BDataType* BTmp = BPtr;
+    CDataType* CTmp = CPtr;
+    for(int i = 0; i < MTile; i += 4)
     {
-        for(int i = 0; i < MTile; i += 4)
-        {
-            //ADataType AData = APtr[i * K + k];
-            v_a_f_i0_4 = _mm_broadcast_ss(APtr + i * K + k);
-            v_a_f_i1_4 = _mm_broadcast_ss(APtr + (i + 1) * K + k);
-            v_a_f_i2_4 = _mm_broadcast_ss(APtr + (i + 2) * K + k);
-            v_a_f_i3_4 = _mm_broadcast_ss(APtr + (i + 3) * K + k);
-            v_a_i0_f_16 = _mm512_broadcast_f32x2(v_a_f_i0_4);
-            v_a_i1_f_16 = _mm512_broadcast_f32x2(v_a_f_i1_4);
-            v_a_i2_f_16 = _mm512_broadcast_f32x2(v_a_f_i2_4);
-            v_a_i3_f_16 = _mm512_broadcast_f32x2(v_a_f_i3_4);
-            for(int j = 0; j < NTile; j += 16)
-            {   
-                v_b_f_16 = _mm512_load_ps(BPtr + k * N + j);
-
-                v_c_i0_f_16 = _mm512_load_ps(CPtr + i * N + j);
-                v_c_i1_f_16 = _mm512_load_ps(CPtr + (i + 1) * N + j);
-                v_c_i2_f_16 = _mm512_load_ps(CPtr + (i + 2) * N + j);
-                v_c_i3_f_16 = _mm512_load_ps(CPtr + (i + 3) * N + j);
-
-                v_c_i0_f_16 = _mm512_fmadd_ps(v_a_i0_f_16, v_b_f_16, v_c_i0_f_16);
-                v_c_i1_f_16 = _mm512_fmadd_ps(v_a_i1_f_16, v_b_f_16, v_c_i1_f_16);
-                v_c_i2_f_16 = _mm512_fmadd_ps(v_a_i2_f_16, v_b_f_16, v_c_i2_f_16);
-                v_c_i3_f_16 = _mm512_fmadd_ps(v_a_i3_f_16, v_b_f_16, v_c_i3_f_16);
-
-                _mm512_store_ps(CPtr + i * N + j, v_c_i0_f_16);
-                _mm512_store_ps(CPtr + (i + 1) * N + j, v_c_i1_f_16);
-                _mm512_store_ps(CPtr + (i + 2) * N + j, v_c_i2_f_16);
-                _mm512_store_ps(CPtr + (i + 3) * N + j, v_c_i3_f_16);
-                //CPtr[i * N + j] += AData * BPtr[k * N + j];
-            }
+        for(int j = 0; j < NTile; j += 32)
+        {   
+            gemm_cpu_micro_tile_4x32x32<ADataType, BDataType, CDataType, AccDataType>(ATmp, BTmp, CTmp, M, N, K);
+            BTmp += 32;
+            CTmp += 32;
         }
+        ATmp += 4 * K;
+        BTmp = BPtr;
+        CTmp += 4 * N - NTile;
     }
 }
 
@@ -75,7 +122,7 @@ void gemm_cpu_opt_tile_l1_cache_avx512(const ADataType* APtr, const BDataType* B
         {
             for(int k = 0; k < K; k += kc)
             {
-                gemm_cpu_macro_tile_4x16<ADataType, 
+                gemm_cpu_macro_tile_M_N_K<ADataType, 
                                          BDataType, 
                                          CDataType, 
                                          AccDataType,
